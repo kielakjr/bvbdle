685eafcc321e8f23b092a1d063dafdba9594350f
mkielak <kielakowsky17@gmail.com>
2025-12-24 13:57:04 +0100

    Added getting random match

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import matches from '../data/database.json';
 
 const App = () => {
+  const [randomMatch, setRandomMatch] = useState(null);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * matches.length);
+    const selectedMatch = matches[randomIndex];
+
+    setRandomMatch(selectedMatch);
+  }, []);
+
+
   return (
-    <div>App</div>
-  )
+    <div>
+      {randomMatch?.date}
+    </div>
+  );
 }
 
-export default App
+export default App;
